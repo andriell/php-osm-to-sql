bc13b32b2dd0c511b28cbb1c482d876cfe56d90d
andriell <root.andriell@gmail.com>
2017-09-14 14:19:42 +0300

    start

---
+++ resources/mysql.sql
@@ -0,0 +1,76 @@
+CREATE TABLE IF NOT EXISTS `node` (
+  `id` bigint(64) NOT NULL,
+  `version` bigint(64) NOT NULL DEFAULT 0,
+  `latitude` double DEFAULT NULL,
+  `longitude` double DEFAULT NULL,
+  `user` varchar(255) DEFAULT NULL,
+  `uid` bigint(64) DEFAULT NULL,
+  `visible` tinyint(1) NOT NULL DEFAULT 1,
+  `timestamp` datetime DEFAULT NULL,
+  `changeset` bigint(64) DEFAULT NULL,
+  PRIMARY KEY (`id`,`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `node_tag` (
+  `node_id` bigint(64) NOT NULL,
+  `k` varchar(255) NOT NULL,
+  `v` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`node_id`,`k`),
+  KEY `k` (`k`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `relation` (
+  `id` bigint(64) NOT NULL,
+  `version` bigint(64) NOT NULL DEFAULT 0,
+  `user` varchar(255) DEFAULT NULL,
+  `uid` bigint(20) DEFAULT NULL,
+  `visible` tinyint(1) NOT NULL DEFAULT 1,
+  `changeset` bigint(64) DEFAULT NULL,
+  `timestamp` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`,`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `relation_member` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `relation_id` bigint(64) NOT NULL,
+  `type` enum('node','way','relation') DEFAULT 'node',
+  `ref` bigint(64) DEFAULT NULL,
+  `role` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `relation_id` (`relation_id`),
+  KEY `ref` (`ref`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `relation_tag` (
+  `relation_id` bigint(64) NOT NULL,
+  `k` varchar(255) NOT NULL,
+  `v` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`relation_id`,`k`),
+  KEY `k` (`k`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `way` (
+  `id` bigint(64) NOT NULL,
+  `version` bigint(64) NOT NULL DEFAULT 0,
+  `visible` tinyint(1) NOT NULL DEFAULT 1,
+  `user` varchar(255) DEFAULT NULL,
+  `uid` bigint(64) DEFAULT NULL,
+  `changeset` bigint(64) DEFAULT NULL,
+  `timestamp` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`,`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `way_node` (
+  `way_id` bigint(64) NOT NULL,
+  `node_id` bigint(64) NOT NULL,
+  PRIMARY KEY (`way_id`,`node_id`),
+  KEY `node_id` (`node_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `way_tag` (
+  `way_id` bigint(64) NOT NULL,
+  `k` varchar(255) NOT NULL,
+  `v` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`way_id`,`k`),
+  KEY `k` (`k`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
