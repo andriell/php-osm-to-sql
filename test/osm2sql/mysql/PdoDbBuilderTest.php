7e9ae5b1c487e8dc663837a78192e2ecbcfa97cc
andriell <root.andriell@gmail.com>
2019-07-12 16:21:44 +0300

    v2.0.9

--- test/osm2sql/mysql/PdoDbBuilderTest.php
+++ test/osm2sql/mysql/PdoDbBuilderTest.php
@@ -8,35 +8,67 @@ use osm2sql\LargeXmlReader;
 
 class PdoDbBuilderTest extends \PHPUnit_Framework_TestCase
 {
-    public function test1()
+
+    private $largeXmlReader;
+    private $listener;
+
+    /**
+     * PdoDbBuilderTest constructor.
+     */
+    public function __construct()
     {
-        $file = __DIR__ . '/resources/RU-MOS.osm';
-        $largeXmlReader = new LargeXmlReader();
-        $listener = new PdoDbBuilder('mysql:host=192.168.99.100;dbname=osm2sql;port=3307', 'root', 'password');
-        $listener->setInsertSize(100);
-        $listener->setInsertIgnore(true);
-        $listener->setProgressListener(function ($size, $total) {
-            echo 'Update DB ' . $size . ' row of ' . $total . " rows\n";
-        });
-        $listener->setExceptionListener(function($e, $sqlStr) {
-            echo $sqlStr . "\n";
-            echo $e . "\n";
-        });
+        parent::__construct();
+        $file = __DIR__ . '/../../../resources/RU-MOS.osm';
+        $this->largeXmlReader = new LargeXmlReader();
+        $this->listener = new PdoDbBuilder('mysql:host=192.168.99.100;dbname=osm2sql;port=3307', 'root', 'password');
+        $this->listener->setInsertSize(100);
+        $this->listener->setInsertIgnore(true);
+        $this->listener->setProgressListener(array($this, 'updateProgressListener'));
+        $this->listener->setExceptionListener(array($this, 'exceptionListener'));
 
-        $largeXmlReader->setFilePath($file);
-        $largeXmlReader->setListener($listener);
-        $largeXmlReader->setProgressListener(function ( $readSize, $totalSize) {
-            echo  'Read file ' . $readSize . ' Mb of ' . $totalSize . " Mb\n";
-        });
-        
-        $largeXmlReader->setStartBlock(2966);
-        $largeXmlReader->parse();
+        $this->largeXmlReader->setFilePath($file);
+        $this->largeXmlReader->setListener($this->listener);
+        $this->largeXmlReader->setProgressListener(array($this, 'readProgressListener'));
+    }
 
-        $listener->deleteHighway();
-        $listener->calculateHighway();
-        $listener->deleteBuilding();
-        $listener->calculateBuilding();
-        $listener->deletePlace();
-        $listener->calculatePlace();
+    public function updateProgressListener($size, $total)
+    {
+        echo 'Update DB ' . $size . ' row of ' . $total . ' rows' . "\n";
+        ob_flush();
+    }
+
+    public function readProgressListener($readSize, $totalSize)
+    {
+        echo 'Read file ' . $readSize . ' Mb of ' . $totalSize . ' Mb' . "\n";
+        ob_flush();
+    }
+
+    public function exceptionListener($e, $sqlStr) {
+        echo $e;
+        echo $sqlStr;
+        ob_flush();
+    }
+
+    public function testClean()
+    {
+        $this->listener->deleteNode();
+        $this->listener->deleteRelation();
+        $this->listener->deleteWay();
+    }
+
+    public function testParse()
+    {
+        $this->largeXmlReader->setStartBlock(0);
+        $this->largeXmlReader->parse();
+    }
+
+    public function testCalculate()
+    {
+        $this->listener->deleteHighway();
+        $this->listener->calculateHighway();
+        $this->listener->deleteBuilding();
+        $this->listener->calculateBuilding();
+        $this->listener->deletePlace();
+        $this->listener->calculatePlace();
     }
 }
